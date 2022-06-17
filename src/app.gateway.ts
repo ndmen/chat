cd76a9c11a8e91ca0a04627c323c4f2637a7a29b
Artem Abdullin <artemleelooai@gmail.com>
2022-06-17 21:21:44 +0300

    Added Logger to AppGateway

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,24 +1,45 @@
 import {
   SubscribeMessage,
   MessageBody,
+  OnGatewayConnection,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayInit,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class AppGateway {
+export class AppGateway
+  implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
+  private logger: Logger = new Logger('AppGateway');
+
   @SubscribeMessage('create_message')
-  async identity(@MessageBody() data: number): Promise<number> {
-    await this.server.sockets.emit('get_message', data);
-    console.log(data);
-    return data;
+  async handleSendMessage(client: Socket, payload: Chat): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log(server);
+    //Do stuffs
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+    // await this.chatService.getUserFromSocket(socket);
   }
 }
