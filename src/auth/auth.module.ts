17278dacb1989e6cf0048fcae40c02318a8c060c
Artem Abdullin <artemleelooai@gmail.com>
2022-06-22 21:57:34 +0300

    Update Jwt expires

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '360s' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AuthController],
